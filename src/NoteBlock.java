32072569adadf4611cad409c1430b029c884e04c
Willem Mulder <willemmaster@hotmail.com>
2011-09-04 23:08:10 +0200

    Furnace wrapper improved, note block wrapper added

---
+++ src/NoteBlock.java
@@ -0,0 +1,59 @@
+/**
+ * Class used for interfacing with note blocks.
+ * @author 14mRh4X0r
+ */
+class NoteBlock implements ComplexBlock {
+
+    OTileEntityNote note;
+
+    public NoteBlock(OTileEntityNote note) {
+        this.note = note;
+    }
+
+    @Override
+    public int getX() {
+        return note.e;
+    }
+
+    @Override
+    public int getY() {
+        return note.f;
+    }
+
+    @Override
+    public int getZ() {
+        return note.g;
+    }
+
+    @Override
+    public void update() {
+        note.i();
+    }
+
+    @Override
+    public Block getBlock() {
+        return getWorld().getBlockAt(getX(), getY(), getZ());
+    }
+
+    @Override
+    public World getWorld() {
+        return note.d.world;
+    }
+
+    /**
+     * Returns the current pitch of the note block.
+     * @return current pitch
+     */
+    public byte getNote() {
+        return note.a;
+    }
+
+    /**
+     * Sets the pitch of the note block to a given value.
+     * @param note The new pitch
+     */
+    public void setNote(byte note) {
+        this.note.a = note;
+    }
+
+}
