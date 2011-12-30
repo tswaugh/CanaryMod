f2b8445b8d49b3385d45c5729a05a96693415751
Yariv Livay <yarivlivay@gmail.com>
2011-12-30 17:52:25 +0200

    Fixed onPlayerRespawn not being called.

--- src/OServerConfigurationManager.java
+++ src/OServerConfigurationManager.java
@@ -198,6 +198,7 @@ public class OServerConfigurationManager {
     }
 
     public OEntityPlayerMP a(OEntityPlayerMP var1, int var2, boolean var3) {
+        etc.getLoader().callHook(PluginLoader.Hook.PLAYER_RESPAWN, ((OEntityPlayerMP)var1).getPlayer());
         this.c.b(var1.w).a(var1);
         this.c.b(var1.w).b(var1);
         this.a(var1.w).b(var1);
