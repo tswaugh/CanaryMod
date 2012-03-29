687201fe115136cf0d0ac4586ffd7943d99e87df
Billy Wands <shadow386@live.com>
2012-03-28 20:59:10 -0400

    Updating small missed things and fixing a checksum derp

--- src/Main.java
+++ src/Main.java
@@ -22,8 +22,8 @@ public class Main {
     public static final long   minecraft_servero = 0xcd12ebd8L;
 
     public static final long   mysql = 0xb2e59524L;
-    public static final long   jarjar = 0x1b1ecc01L;
-    public static final long   rules = 0xAC13A09EL;
+    public static final long   jarjar = 0x0de915d3L;
+    public static final long   rules = 0x1b1ecc01L;
 
     public static final Logger log = Logger.getLogger("Minecraft");
     
