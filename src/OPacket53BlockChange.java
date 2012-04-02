015a8331c549b0a774adb80e0a078d612e7011c7
Billy Wands <shadow386@live.com>
2012-04-02 17:03:40 -0400

    Fixing anti-xray blocks disappearing until relog, DOES NOT FIX ANTIXRAY THOUGH

--- src/OPacket53BlockChange.java
+++ src/OPacket53BlockChange.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 
 
 public class OPacket53BlockChange extends OPacket {
+
     public int a;
     public int b;
     public int c;
@@ -11,86 +12,46 @@ public class OPacket53BlockChange extends OPacket {
     public int e;
 
     public OPacket53BlockChange() {
+        super();
         this.p = true;
     }
 
-    public OPacket53BlockChange(int paramInt1, int paramInt2, int paramInt3, OWorld paramOWorld) {
+    public OPacket53BlockChange(int var1, int var2, int var3, OWorld var4) {
+        super();
         this.p = true;
-        this.a = paramInt1;
-        this.b = paramInt2;
-        this.c = paramInt3;
-    
-        // CanaryMod start
-        if (etc.getInstance().isAntiXRayEnabled()) {
-            OChunk activeChunk = paramOWorld.c(a, c);
-        
-            int blockID = paramOWorld.a(paramInt1, paramInt2, paramInt3);
-
-            // If we are attemting to send a hidden block
-            if (etc.getDataSource().getAntiXRayBlocks().contains(new Integer(blockID))) {
-                Integer index = new Integer((paramInt1 & 0xf) << 11 | (paramInt3 & 0xf) << 7 | paramInt2);
-
-                /* If the block is supposed to be hidden
-                if (activeChunk.antiXRayBlocks.containsKey(index)) {
-                    if (activeChunk.isHidden(paramInt1 & 0xf, paramInt2, paramInt3 & 0xf)) {
-                        // Send it as a stone.
-                        this.d = 1;
-                        this.e = 0;
-                    } else {
-                        this.d = blockID;
-                        this.e = paramOWorld.c(paramInt1, paramInt2, paramInt3);
-                    }
-                } else {
-                    // This should never happen. It means the cache is not complete!
-                    synchronized (activeChunk.antiXRayBlocksLock) {
-                        activeChunk.antiXRayBlocks.put(index, new Integer(blockID));
-                    }
-                    if (activeChunk.isHidden(paramInt1 & 0xf, paramInt2, paramInt3 & 0xf)) {
-                        // Sent it as a stone.
-                        this.d = 1;
-                        this.e = 0;
-                    } else {
-                        this.d = blockID;
-                        this.e = paramOWorld.c(paramInt1, paramInt2, paramInt3);
-                    }
-                }*/
-            } else {
-                this.d = blockID;
-                this.e = paramOWorld.c(paramInt1, paramInt2, paramInt3);
-            }
-        } else {
-            this.d = paramOWorld.a(paramInt1, paramInt2, paramInt3);
-            this.e = paramOWorld.c(paramInt1, paramInt2, paramInt3);
-        }
-        // CanaryMod end
+        this.a = var1;
+        this.b = var2;
+        this.c = var3;
+        this.d = var4.a(var1, var2, var3);
+        this.e = var4.c(var1, var2, var3);
     }
 
-    public void a(DataInputStream paramDataInputStream) {
+    public void a(DataInputStream var1) {
+    	try {
+    		this.a = var1.readInt();
+    		this.b = var1.read();
+    		this.c = var1.readInt();
+    		this.d = var1.read();
+    		this.e = var1.read();
+    	} catch (IOException e1) {
+    		e1.printStackTrace();
+    	}
+    }
+
+    public void a(DataOutputStream var1) {
         try {
-            this.a = paramDataInputStream.readInt();
-            this.b = paramDataInputStream.read();
-            this.c = paramDataInputStream.readInt();
-            this.d = paramDataInputStream.read();
-            this.e = paramDataInputStream.read();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+			var1.writeInt(this.a);
+			var1.write(this.b);
+			var1.writeInt(this.c);
+			var1.write(this.d);
+			var1.write(this.e);
+		} catch (IOException e1) {
+			e1.printStackTrace();
+		}
     }
 
-    public void a(DataOutputStream paramDataOutputStream) {
-        try {
-            paramDataOutputStream.writeInt(this.a);
-            paramDataOutputStream.write(this.b);
-            paramDataOutputStream.writeInt(this.c);
-            paramDataOutputStream.write(this.d);
-            paramDataOutputStream.write(this.e);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-
-    public void a(ONetHandler paramONetHandler) {
-        paramONetHandler.a(this);
+    public void a(ONetHandler var1) {
+        var1.a(this);
     }
 
     public int a() {
