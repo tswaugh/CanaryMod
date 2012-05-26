ca74877946b79145f7ddc0f2cfcc8ef95d2c870d
Brian McCarthy <brianmccarthy@windowslive.com>
2012-05-26 12:26:52 +1200

    Adding Wolf control and JavaDocs to sheep and wolf.

--- src/Sheep.java
+++ src/Sheep.java
@@ -1,10 +1,26 @@
-
+/**
+ * Wrap around an OEntitySheep to provide extra methods not available to anything but sheep.
+ * Mostly wool options.
+ * 
+ * @author Brian McCarthy
+ *
+ */
 public class Sheep extends Mob {
 
+	/**
+	 * Basic sheep constructor.
+	 * 
+	 * @param entity An instance of OEntitySheep to wrap around.
+	 */
 	public Sheep(OEntitySheep entity) {
 		super(entity);
 	}
 
+	/**
+	 * Set if the sheep has wool.
+	 * 
+	 * @param sheared Sheared or not
+	 */
 	public void setSheared(boolean sheared){
 		int i = getEntity().bY.a(16);
 		if (sheared){
@@ -14,17 +30,41 @@ public class Sheep extends Mob {
 		}
 	}
 
+	/**
+	 * Returns if the sheep has wool.
+	 * 
+	 * @return If this sheep has wool.
+	 */
 	public boolean isSheared(){
 		return (getEntity().bY.a(16) & 0x10) != 0;
 	}
 
+	/**
+	 * Set the color of the wool.
+	 * 
+	 * @param color int value of the new color.
+	 */
 	public void setWoolColor(int color){
 		int i = getEntity().bY.a(16);
 		getEntity().bY.b(16, Byte.valueOf((byte)(i & 0xF0 | color & 0xF)));
 	}
 
+	/**
+	 * Returns an int of the color of the wool.
+	 * 
+	 * @return int value of wool color.
+	 */
 	public int getColor(){
 		return getEntity().bY.a(16) & 0xF;
+	}
+
+	/**
+	 * Returns a Cloth.Color value of the wool.
+	 * 
+	 * @return Cloth.Color of the wool. This is useful for getting the name of the wool.
+	 */
+	public Cloth.Color getClothColor(){
+		return Cloth.Color.getColor(getColor());
 	}
 
 	public OEntitySheep getEntity() {
