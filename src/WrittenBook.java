01b44397667daac5860337b7e0f1301d6a24ac0b
Willem Mulder <willemmaster@hotmail.com>
2012-10-11 14:25:08 +0200

    Clean up documentation, fix iValid to isValid

--- src/WrittenBook.java
+++ src/WrittenBook.java
@@ -9,93 +9,113 @@ import java.util.List;
  */
 public class WrittenBook {
 
-	OItemStack book;
+    OItemStack book;
 
-	/**
-	 * Takes an item - this should be a instance of a book and quill or written book
-	 * Use isValid() to make sure the item is a book
-	 * Theoretically it is possible for any item to contain the correct tags that make it seam like a book
-	 * However this can not happen with out 3rd party interference 
-	 * @param item
-	 * @see #isValid()
-	 */
-	public WrittenBook(Item item) {
-		this.book = item.getBaseItem();
-	}
-	
-	/**
-	 * Takes the OItemStack for this book
-	 * @param book
-	 */
-	public WrittenBook(OItemStack book) {
-		this.book = book;
-	}
+    /**
+     * Creates a new instance of WrittenBook.
+     * Use {@link isValid()} to make sure the item is a book. Theoretically it
+     * is possible for any item to contain the correct tags that make it seem
+     * like a book, although this does not occur "naturally".
+     * @param item An instance of a book and quill or a written book.
+     */
+    public WrittenBook(Item item) {
+        this.book = item.getBaseItem();
+    }
 
-	/**
-	 * Get the title of this book
-	 * @return empty string ("") if the NBTTagCompound is null or the NBTTagCompund does not contain the "title" tag (usually when item is not a book)
-	 */
-	public String getTitle() {
-		if (book.d == null) {
-			return "";
-		}
-		if (!book.d.b("title")) {
-			return "";
-		}
-		return book.d.i("title");
-	}
-	
-	public void setTitle(String title) {
-		book.d.a("title", title);
-	}
+    /**
+     * Takes the OItemStack for this book.
+     * @param book The book's OItemStack
+     */
+    public WrittenBook(OItemStack book) {
+        this.book = book;
+    }
 
-	/**
-	 * Get the author of this book
-	 * @return empty string ("") if the NBTTagCompound is null or the NBTTagCompund does not contain the "author" tag (usually when item is not a book)
-	 */
-	public String getAuthor() {
-		if (book.d == null) {
-			return "";
-		}
-		if (!book.d.b("author")) {
-			return "";
-		}
-		return book.d.i("author");
-	}
-	
-	public void setAuthor(String author) {
-		book.d.a("author", author);
-	}
+    /**
+     * Get the title of this book.
+     * @return empty string ("") if the <tt>NBTTagCompound</tt> is
+     * <tt>null</tt> or if it does not contain the "title" tag (usually when
+     * item is not a book)
+     */
+    public String getTitle() {
+        if (book.d == null) {
+            return "";
+        }
+        if (!book.d.b("title")) {
+            return "";
+        }
+        return book.d.i("title");
+    }
 
-	public List<String> getPages() {
-		if (book.d == null) {
-			return Collections.emptyList();
-		}
-		List<String> pages = new ArrayList<String>();
-		ONBTTagList nbtPages = (ONBTTagList) book.d.a("pages");
-		for (int i = 0; i < nbtPages.c(); i++) {
-			ONBTTagString nbtPage = (ONBTTagString) nbtPages.b(i);
-			String page = nbtPage.a;
-			if (page != null && !page.trim().equals("")) {
-				pages.add(page);
-			}
-		}
-		return pages;
-	}
-	
-	public void setPages(List<String> pages) {
-		ONBTTagList nbtPages = new ONBTTagList("pages");
-		for (String page : pages) {
-			nbtPages.a(new ONBTTagString(page));
-		}
-		book.d.a("pages", nbtPages);
-	}
-	
-	public boolean iValid() {
-		if (book.c == 386 || book.c == 387) {
-			return true;
-		}
-		return false;
-	}
+    public void setTitle(String title) {
+        book.d.a("title", title);
+    }
+
+    /**
+     * Get the author of this book.
+     * @return empty string ("") if the <tt>NBTTagCompound</tt> is
+     * <tt>null</tt> or if it does not contain the "author" tag (usually when
+     * item is not a book)
+     */
+    public String getAuthor() {
+        if (book.d == null) {
+            return "";
+        }
+        if (!book.d.b("author")) {
+            return "";
+        }
+        return book.d.i("author");
+    }
+
+    /**
+     * Set the author of this book.
+     * @param author The new author
+     */
+    public void setAuthor(String author) {
+        book.d.a("author", author);
+    }
+
+    /**
+     * Get the pages of this book.
+     * @return A <tt>List&lt;String&gt;</tt> containing the pages, one page per
+     * item.
+     */
+    public List<String> getPages() {
+        if (book.d == null) {
+            return Collections.emptyList();
+        }
+        List<String> pages = new ArrayList<String>();
+        ONBTTagList nbtPages = (ONBTTagList) book.d.a("pages");
+        for (int i = 0; i < nbtPages.c(); i++) {
+            ONBTTagString nbtPage = (ONBTTagString) nbtPages.b(i);
+            String page = nbtPage.a;
+            if (page != null && !page.trim().equals("")) {
+                pages.add(page);
+            }
+        }
+        return pages;
+    }
+
+    /**
+     * Set the pages of this book.
+     * @param pages A list containing the new pages as a <tt>String</tt>
+     */
+    public void setPages(List<String> pages) {
+        ONBTTagList nbtPages = new ONBTTagList("pages");
+        for (String page : pages) {
+            nbtPages.a(new ONBTTagString(page));
+        }
+        book.d.a("pages", nbtPages);
+    }
+
+    /**
+     * Check if this book is valid.
+     * @return <tt>true</tt> when this is really a book, <tt>false</tt> otherwise.
+     */
+    public boolean isValid() {
+        if (book.c == 386 || book.c == 387) {
+            return true;
+        }
+        return false;
+    }
 
 }
