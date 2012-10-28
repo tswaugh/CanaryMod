c0aae205f4f68333b58136a499f1b877a72823c5
Billy Wands <shadow386@live.com>
2012-10-28 13:39:40 -0400

    Accidentally deleted file

---
+++ src/OEnchantmentModifierDamage.java
@@ -0,0 +1,15 @@
+final class OEnchantmentModifierDamage implements OIEnchantmentModifier {
+
+    public int a;
+    public ODamageSource b;
+
+    private OEnchantmentModifierDamage() {}
+
+    public void a(OEnchantment oenchantment, int i) {
+        this.a += oenchantment.a(i, this.b);
+    }
+
+    OEnchantmentModifierDamage(OEmpty3 oempty3) {
+        this();
+    }
+}
