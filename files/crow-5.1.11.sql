fc941f556359d99725851b8db969934e9c5caed8
Billy Wands <shadow386@live.com>
2012-11-04 12:28:48 -0500

    Updating checksum for updated servero

--- files/crow-5.1.11.sql
+++
@@ -1,6 +0,0 @@
-INSERT INTO  `minecraft`.`items` (`name` ,`itemid`)
-
-VALUES ('bookandquill',  386),('writtenbook',  387),('emerald',  388),
-('woodendoubleslab', 125),('woodenslab', 126),('cocoaplant', 127),
-('sandstonetairs', 128),('emeraldore',  129),('enderchest',  130),
-('tripwirehook',  131),('emeraldblock',  133);
