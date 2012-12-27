1f1ecef31e3a8c3320ba7ae4205fb59f3834597e
Billy Wands <shadow386@live.com>
2012-12-26 23:13:25 -0500

    Moving fix up a couple lines

--- src/OItemFirework.java
+++ src/OItemFirework.java
@@ -6,11 +6,10 @@ public class OItemFirework extends OItem {
 
     public boolean a(OItemStack oitemstack, OEntityPlayer oentityplayer, OWorld oworld, int i, int j, int k, int l, float f, float f1, float f2) {
         if (!oworld.I) {
-        	OEntityFireworkRocket oentityfireworkrocket = new OEntityFireworkRocket(oworld, (double) ((float) i + f), (double) ((float) j + f1), (double) ((float) k + f2), oitemstack);
-        	
         	if(oitemstack.d == null)
         		return true;
-            
+        	
+        	OEntityFireworkRocket oentityfireworkrocket = new OEntityFireworkRocket(oworld, (double) ((float) i + f), (double) ((float) j + f1), (double) ((float) k + f2), oitemstack);
         	oworld.d((OEntity) oentityfireworkrocket);
             if (!oentityplayer.cd.d) {
                 --oitemstack.a;
