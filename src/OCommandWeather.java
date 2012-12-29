55139e78495c0a4a850450296575991052460770
Brian McCarthy <brianmccarthy@windowslive.com>
2012-12-29 16:07:06 +1300

    Fixed OCommandWeather.java and OServerCommandManager.java to work with multi world. Work on #84 other commands need to be checked. This will not work in other worlds but I don't think that can be done without passing in more arguments.

---
+++ src/OCommandWeather.java
@@ -0,0 +1,50 @@
+import java.util.List;
+import java.util.Random;
+
+public class OCommandWeather extends OCommandBase {
+
+    public OCommandWeather() {}
+
+    public String c() {
+        return "weather";
+    }
+
+    public int a() {
+        return 2;
+    }
+
+    public void b(OICommandSender oicommandsender, String[] astring) {
+        if (astring.length < 1) {
+            throw new OWrongUsageException("commands.weather.usage", new Object[0]);
+        } else {
+            int i = (300 + (new Random()).nextInt(600)) * 20;
+
+            if (astring.length >= 2) {
+                i = a(oicommandsender, astring[1], 1, 1000000) * 20;
+            }
+
+            OWorldServer oworldserver = OMinecraftServer.D().worlds.get(etc.getServer().getDefaultWorld().getName())[0]; // CanaryMod - multiworld fix
+            OWorldInfo oworldinfo = oworldserver.K();
+
+            oworldinfo.g(i);
+            oworldinfo.f(i);
+            if ("clear".equalsIgnoreCase(astring[0])) {
+                oworldinfo.b(false);
+                oworldinfo.a(false);
+                a(oicommandsender, "commands.weather.clear", new Object[0]);
+            } else if ("rain".equalsIgnoreCase(astring[0])) {
+                oworldinfo.b(true);
+                oworldinfo.a(false);
+                a(oicommandsender, "commands.weather.rain", new Object[0]);
+            } else if ("thunder".equalsIgnoreCase(astring[0])) {
+                oworldinfo.b(true);
+                oworldinfo.a(true);
+                a(oicommandsender, "commands.weather.thunder", new Object[0]);
+            }
+        }
+    }
+
+    public List a(OICommandSender oicommandsender, String[] astring) {
+        return astring.length == 1 ? a(astring, new String[] { "clear", "rain", "thunder"}) : null;
+    }
+}
---
+++ src/OServerCommandManager.java
@@ -0,0 +1,75 @@
+import java.util.Iterator;
+
+public class OServerCommandManager extends OCommandHandler implements OIAdminCommand {
+
+    public OServerCommandManager() {
+        this.a(new OCommandTime());
+        this.a(new OCommandGameMode());
+        this.a(new OCommandDifficulty());
+        this.a(new OCommandDefaultGameMode());
+        this.a(new OCommandKill());
+        this.a(new OCommandToggleDownfall());
+        this.a(new OCommandWeather());
+        this.a(new OCommandXP());
+        this.a(new OCommandServerTp());
+        this.a(new OCommandGive());
+        this.a(new OCommandEnchant());
+        this.a(new OCommandServerEmote());
+        this.a(new OCommandShowSeed());
+        this.a(new OCommandHelp());
+        this.a(new OCommandDebug());
+        this.a(new OCommandServerMessage());
+        this.a(new OCommandServerSay());
+        this.a(new OCommandSetSpawnpoint());
+        this.a(new OCommandGameRule());
+        this.a(new OCommandClearInventory());
+        if (OMinecraftServer.D().T()) {
+            this.a(new OCommandServerOp());
+            this.a(new OCommandServerDeop());
+            this.a(new OCommandServerStop());
+            this.a(new OCommandServerSaveAll());
+            this.a(new OCommandServerSaveOff());
+            this.a(new OCommandServerSaveOn());
+            this.a(new OCommandServerBanIp());
+            this.a(new OCommandServerPardonIp());
+            this.a(new OCommandServerBan());
+            this.a(new OCommandServerBanlist());
+            this.a(new OCommandServerPardon());
+            this.a(new OCommandServerKick());
+            this.a(new OCommandServerList());
+            this.a(new OCommandServerWhitelist());
+        } else {
+            this.a(new OCommandServerPublishLocal());
+        }
+
+        OCommandBase.a((OIAdminCommand) this);
+    }
+
+    public void a(OICommandSender oicommandsender, int i, String s, Object... aobject) {
+        boolean flag = true;
+
+        if (oicommandsender instanceof OTileEntityCommandBlock && !OMinecraftServer.D().worlds.get(etc.getServer().getDefaultWorld().getName())[0].L().b("commandBlockOutput")) { // CanaryMod - multiworld fix
+            flag = false;
+        }
+
+        if (flag) {
+            Iterator iterator = OMinecraftServer.D().ad().b.iterator();
+
+            while (iterator.hasNext()) {
+                OEntityPlayerMP oentityplayermp = (OEntityPlayerMP) iterator.next();
+
+                if (oentityplayermp != oicommandsender && OMinecraftServer.D().ad().e(oentityplayermp.bR)) {
+                    oentityplayermp.a("\u00A77\u00A7o[" + oicommandsender.c_() + ": " + oentityplayermp.a(s, aobject) + "]");
+                }
+            }
+        }
+
+        if (oicommandsender != OMinecraftServer.D()) {
+            OMinecraftServer.a.info("[" + oicommandsender.c_() + ": " + OMinecraftServer.D().a(s, aobject) + "]");
+        }
+
+        if ((i & 1) != 1) {
+            oicommandsender.a(oicommandsender.a(s, aobject));
+        }
+    }
+}
